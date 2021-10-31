0159135ddbb8aaf6571af15c2d295fed38dfe4ce
Lêɠêɳ̃dẞøy <84609111+LEGEND-OS@users.noreply.github.com>
2021-10-31 21:06:01 +0530

    Update raid.py

--- raid.py
+++ raid.py
@@ -379,7 +379,7 @@ async def _(event):
     async with event.client.action(event.chat_id, "typing"):
         await event.client.send_message(
             entity=event.chat_id,
-            message="""{}""".format(random.choice(RRAID)),
+            message="""{}""".format(random.choice(RAID)),
             reply_to=event.message.id,
         )
 
